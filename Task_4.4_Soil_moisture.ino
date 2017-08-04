6ac2d23f488ad3d354e6818c58ff30c32355c134
mehulwarade <mwarade@deakin.edu.au>
2017-08-04 11:44:05 +1000

    Add files via upload

---
+++ Task_4.4_Soil_moisture.ino
@@ -0,0 +1,34 @@
+/*
+  # Editor     : Lauren
+  # Date       : 13.01.2012
+  # Version    : 1.0
+  # Connect the sensor to the A0(Analog 0) pin on the Arduino board
+  # the sensor value description
+  # 0  ~300     dry soil
+  # 300~700     humid soil
+  # 700~950     in water
+  */
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600); // open serial port, set the baud rate as 9600 bps
+  
+}
+void loop() {
+  int val;
+  val = analogRead(0); //connect sensor to Analog 0
+   Serial.println(val) ;
+   delay(1000);
+             if(val < 300){
+             digitalWrite(LED_BUILTIN, HIGH);                  
+            }
+             else if(val > 300 && val < 700){
+             digitalWrite(LED_BUILTIN, LOW);    
+            }
+              else {
+                digitalWrite(LED_BUILTIN, HIGH);   
+                delay(300);                      
+                digitalWrite(LED_BUILTIN, LOW); 
+                delay(300);   
+             }
+  }
+
